c6fb2a2fe4c90c8ecc8aa33485b83d4fec307fbc
khabar0val <68781173+khabar0val@users.noreply.github.com>
2021-08-18 01:15:09 +0300

    savva's bot false --rolesdone

---
+++ bot.py
@@ -0,0 +1,59 @@
+import discord
+import config
+
+from discord import utils
+
+intents = discord.Intents.all()
+intents.members = True
+
+class MyClient(discord.Client):
+	async def on_ready(self):
+		print("Logged on as {0}!".format(self.user))
+
+	async def on_raw_reaction_add(self, payload):
+		channel = self.get_channel(payload.channel_id)
+		message = await channel.fetch_message(payload.message_id)
+		member = utils.get(message.guild.members, id=payload.user_id)
+
+		try:
+			emoji = str(payload.emoji)
+			role = utils.get(message.guild.roles, id=config.ROLES[emoji])
+
+			if len([i for i in member.roles if i.id not in config.EXCROLES]) <= config.MAX_ROLES_PER_USER:
+				await member.add_roles(role)
+				print('[SUCCESS] User {0.display_name} has been granted with role {1.name}'.format(member, role))
+
+			else:
+				await message.remove_reaction(payload.emoji, memeber)
+				print('[ERROR] Too many roles for user {0.display_name}' + emoji.format(member, role))
+
+		except KeyError as e:
+			print('[ERROR] KeyError, no role found for' + emoji)
+
+		except Exception as e:
+			print(repr(e))
+
+	async def on_raw_reaction_remove(self, payload):
+		channel = self.get_channel(payload.channel_id)
+		message = await channel.fetch_message(payload.message_id)
+		member = utils.get(message.guild.members, id=payload.user_id)
+
+		try:
+			emoji = str(payload.emoji)
+			role = utils.get(message.guild.roles, id=config.ROLES[emoji])
+
+			await member.remove_role(role)
+			print('[SUCCESS] Role {1.name} has been remove for user {0.display_name}'.format(member, role))
+
+		except KeyError as e:
+			print('[ERROR] KeyError, no role found for' + emoji)
+
+		except Exception as e:
+			print(repr(e))
+
+	client = discord.Client(intents=intents)
+
+# RUN
+
+client = MyClient(intents=discord.Intents.all())
+client.run(config.TOKEN)
---
+++ config.py
@@ -0,0 +1,22 @@
+# token
+
+TOKEN = 'ODc3Mjg4NDIyNjQzOTkwNTI5.YRwcgw.n6TXqrhERZ7ygm69cYHVH1UlpVo'
+
+# post id to read reactions form
+
+POST_ID = 877298484754137138
+
+# roles list according to emoji
+
+ROLES = {
+	'🤣': 877294850263896144, # мемщик
+	'💣🔫': 877294982875205713 # cs
+}
+
+# exclude this roles from counting
+
+EXCROLES = ()
+
+# max amount of roles a user can have
+
+MAX_ROLES_PER_USER = 1
